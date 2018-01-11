453e2be960a6a16622f3b4799daa10329c578625
chisl <info@chisl.io>
2018-01-11 12:09:43 +0100

    Initial commit

---
+++ IIS2MDC.cpp
@@ -0,0 +1,18 @@
+/*
+ * name:        IIS2MDC
+ * description: High-accuracy, ultra-low-power, 3-axis digital output magnetometer
+ * manuf:       STMicroelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/iis2mdc.pdf
+ * date:        2018-01-11
+ * author       https://chisl.io/
+ * file:        IIS2MDC.cpp
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+#include "IIS2MDC.hpp"
+
